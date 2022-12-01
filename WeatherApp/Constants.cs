7b90c539e82664533e4534cb825157007041c0b7
Facundo Montenegro <facumonte29@gmail.com>
2022-12-01 14:58:59 -0300

    Agregar archivos de proyecto.

---
+++ WeatherApp/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    public static class Constants
+    {
+        public static string OpenWheatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public static string OpenWheatherMapAPIKEY = "3c6b4934ba87e94df85e9fd81e5c9f80";
+       
+
+    }
+}
---
+++ WeatherApp/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace WeatherApp;
+
+public partial class MainPage : ContentPage
+{
+	
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+	}
+}
+
